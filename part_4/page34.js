0502f2918c620cb77d3a8b8e8127a4c33fba45e3
shivaganesh720 <naguruganesh2005@gmail.com>
2026-03-06 13:38:11 +0530

    updated

--- part_4/page34.js
+++ part_4/page34.js
@@ -1,22 +1,44 @@
-import express from "express"
-const app = express()
-app.listen(8080, () => console.log("Server Started"));
-app.set("view engine", "ejs")
-app.set("Views", "Views")
+import express from "express";
+const app = express();
+app.listen(8080, () => console.log("Server Started on port 8080"));
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.listen(5000, () => console.log("Server Started on port 5000"));
 
-app.listen(5000, () => console.log("Server Started"));
+app.use(express.urlencoded({ extended: true }));
 
-const users = [
-    { id: 1, name: "shivaganesh", email: "shivaganesh@gmail.com", password: "1234" },
-    { id: 2, name: "Venu", email: "venu@gmail.com", password: "1234" },
-    { id: 3, name: "Vivek", email: "Vivek@gmail.com", password: "1234" }
+let users = [
+    { name: "Shivaganesh", email: "shivaganesh@gmail.com", password: "1234" },
+    { name: "Venu", email: "venu@gmail.com", password: "1234" },
+    { name: "Somesh", email: "somesh@gmail.com", password: "1234" },
 ];
+
 app.get("/login", (req, res) => {
-    res.render("login");
+    res.render("login", { error: null });
+});
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const user = users.find((user) => user.email === email);
+    if (user) {
+        if (user.password === password) {
+            res.redirect("/");
+        } else {
+            res.render("login", { error: "Invalid Password" });
+        }
+    } else {
+        res.render("login", { error: "User not found" });
+    }
+    // res.redirect("/");
 });
 
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    users = [...users, req.body];
+    res.redirect("/");
 });
 
 app.get("/", (req, res) => {
