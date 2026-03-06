fc66655962e5e1e28474d54aac25158851e68cce
shivaganesh720 <naguruganesh2005@gmail.com>
2026-03-06 10:41:52 +0530

    updated

--- part_4/page34.js
+++ part_4/page34.js
@@ -3,10 +3,22 @@ const app = express()
 app.listen(8080, () => console.log("Server Started"));
 app.set("view engine", "ejs")
 app.set("Views", "Views")
+
+app.listen(5000, () => console.log("Server Started"));
+
+const users = [
+    { username: "shivaganesh", email: "shivaganesh@gmail.com", password: "1234"},
+    { username: "Venu", email: "venu@gmail.com", password: "1234" },
+    { username: "Vivek", email: "Vivek@gmail.com", password: "1234" }
+];
 app.get("/login", (req, res) => {
     res.render("login");
 });
 
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+app.get("/", (req, res) => {
+    res.render("dashboard");
 });
